608368d2f695a9b99f009055770e7ec626ed6426
Artur <abezpalyi@justcoded.com>
2022-01-20 15:44:43 +0200

    question 2

--- src/js/modules/vueApp.js
+++ src/js/modules/vueApp.js
@@ -10,27 +10,59 @@ const vueApp = () => {
       el: '#app',
       template:
         `<div class="vue-app-wrapper"> 
-            <div v-for="el in sortHiddenPosts" :key="el.id" class="card">  
+            <form >
+                <div>
+                <input 
+                 v-model="inputValue"
+                 type="text">
+                <select v-model="filteredValue" >
+                  <option>userId</option>
+                  <option>status</option>
+                </select>
+                <div v-if="filteredValue === 'userId'">
+                    <span>author List:</span>
+                   <ul>
+                     <li v-for="author in authors" @click="test(author)" class="author__list-item">{{author}}</li>
+                  </ul>
+                </div>
+                </div>
+            </form>
+            <div class="cards_wrapper">
+            <div
+             v-for="el in sortHiddenPosts"
+             :key="el.id"
+             class="card"
+            >  
                 <img :src="setImage(el)" alt="some image"> 
                 <h2>{{el.title}}</h2> 
-                <h2>{{el.status}}</h2>  <!--   для наглядности работы фильтра -->
-            </div> 
+                <h2>{{el.userId}}</h2>  <!--   для наглядности работы фильтра -->
+            </div>
+            </div>
         </div>`,
-
       data() {
         return {
-
+          posts:[],
+          userPost: {},
+          filteredValue: 'userId',
+          inputValue: '',
+          author: '',
+          authors: posts.reduce((unique, item) => unique.includes(item.userId) ? unique : [...unique, item.userId], []),
         };
       },
       methods: {
-        isTest() {
-        },
         setImage(post) {
           return post.imgUrl || post.thumbnailUrl;
         },
+        test(author) {
+          this.inputValue = String(author);
+          console.log(this.posts)
+        },
       },
       computed: {
-        sortHiddenPosts: () => posts.filter(el => el.status !== 'hidden'),
+        sortHiddenPosts() {
+          if (!this.inputValue) return posts.filter(el => el.status !== 'hidden');
+          return posts.filter(el => String(el[this.filteredValue]) === this.inputValue);
+        },
       },
     });
   }
