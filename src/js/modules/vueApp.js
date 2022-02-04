0104e94191a3e9cbde64ee9bf92f0b0e018bee4c
Artur <abezpalyi@justcoded.com>
2022-02-04 18:38:56 +0200

    fixed bugs

--- src/js/modules/vueApp.js
+++ src/js/modules/vueApp.js
@@ -12,6 +12,7 @@ const vueApp = () => {
   // eslint-disable-next-line no-new
   new Vue({
     el: '#app',
+
     data() {
       return {
         searchParams: null,
@@ -24,77 +25,93 @@ const vueApp = () => {
         urlParams: [],
       };
     },
+
     mixins: [validationMixin],
+
     validations: {
       titleSearch: {
         minLength: minLength(1),
         hasInputValidation,
       },
     },
+
     created() {
       this.getPostsFromServer();
       this.getParams();
       this.bindUrlParams();
     },
+
     methods: {
       bindUrlParams() {
         if (this.searchParams.get('userId')) {
-          this.authorFilterList = [...this.searchParams.get('userId').split(',')].map((el) => Number(el));
+          this.authorFilterList = this.searchParams.get('userId').split(',').map((el) => Number(el));
         }
+
         if (this.searchParams.get('query')) {
           this.titleSearch = this.searchParams.get('query');
         }
       },
+
       getParams() {
         this.searchParams = new URLSearchParams(window.location.search);
       },
+
       getImage(post) {
-        return post.imgUrl || post.thumbnailUrl  || '../images/default.jpg';
+        return post.imgUrl || post.thumbnailUrl || '../images/default.jpg';
       },
+
       getPostsFromServer() {
         fetch('https://jsonplaceholder.typicode.com/posts')
           .then((res) => res.json())
           .then((data) => {
-            this.posts = [...data];
+            this.posts = data;
           })
           .catch((err) => {
             throw err;
           });
       },
+
       deleteFilterHandler(filterItem) {
         this.authorFilterList = this.authorFilterList.filter((el) => el !== filterItem);
       },
+
       urlParamsHandler() {
         this.defaultSearchParam.forEach((param) => {
           this.searchParams.delete(param);
+
           if (this.titleSearch) {
             this.searchParams.delete('query');
             this.searchParams.append('query', this.titleSearch);
           }
+
           if (this.authorFilterList.length) {
             this.searchParams.delete('userId');
             this.searchParams.append('userId', this.authorFilterList.join(','));
           }
         });
+
         window.history.pushState({ path: `?${this.searchParams.toString()}` }, '', `?${this.searchParams.toString()}`);
       },
-      checkBoxUrlParam(){
 
+      checkBoxUrlParam() {
         this.defaultSearchParam.forEach((param) => {
-          this.searchParams.delete(param);
+          this.searchParams.delete('userId');
+
           if (this.authorFilterList.length) {
-            this.searchParams.delete('userId');
             this.searchParams.append('userId', this.authorFilterList.join(','));
           }
         });
+
         window.history.pushState({ path: `?${this.searchParams.toString()}` }, '', `?${this.searchParams.toString()}`);
-      }
+      },
     },
+
     computed: {
       getAuthors() {
         return this.posts.reduce((unique, item) => (unique.includes(item.userId) ? unique
           : [...unique, item.userId]), []);
       },
+
       filteredPosts() {
         return this.posts.filter((el) => el.title.includes(this.titleSearch));
       },
